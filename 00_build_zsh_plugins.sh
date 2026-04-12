01b7869d3f24028565a45f45d9f2d871ec05cfee
Aden Mann <adenmann@tamu.edu>
2026-04-11 20:27:54 -0500

    Hotfix: Fixed ZSH

--- 00_build_zsh_plugins.sh
+++ 00_build_zsh_plugins.sh
@@ -7,12 +7,12 @@ mkdir -p "source_modifications/roofs/usr/share/zsh-plugins"
 
 # Clone the repositories
 git clone https://github.com/zsh-users/zsh-autosuggestions \
-    "source_modifications/roofs/usr/share/zsh-plugins/zsh-autosuggestions"
+    "source_modifications/rootfs/usr/share/zsh-plugins/zsh-autosuggestions"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting \
-    "source_modifications/roofs/usr/share/zsh-plugins/zsh-syntax-highlighting"
+    "source_modifications/rootfs/usr/share/zsh-plugins/zsh-syntax-highlighting"
 
 git clone https://github.com/marlonrichert/zsh-autocomplete \
-    "source_modifications/roofs/usr/share/zsh-plugins/zsh-autocomplete"
+    "source_modifications/rootfs/usr/share/zsh-plugins/zsh-autocomplete"
 
 echo "Installed zsh plugins."
